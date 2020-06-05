0480ef3034f712f1a3e0685e67d996908bb671ec
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-04 22:37:30 -0400

    wrote failing tests for recipe search and recipe info

--- src/pages/RecipeInfo/RecipeInfo.test.js
+++ src/pages/RecipeInfo/RecipeInfo.test.js
@@ -2,10 +2,25 @@ import React from "react";
 import RecipeInfo from "./RecipeInfo";
 import {HashRouter as Router} from "react-router-dom";
 import {render, fireEvent} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 describe("<RecipeInfo/>" , () => {
     test("User is able toggle between US and metric measurements", () => {
+        const {getByTestId, getByText} = render(
+            <Router>
+                <RecipeInfo/>
+            </Router>
+        );
 
+        expect(getByText("Measurement System : US")).toBeInTheDocument();
+
+        fireEvent.click(getByTestId("toggleMeasurementSystemButton"));
+
+        expect(getByText("Measurement System : Metric")).toBeInTheDocument();
+
+        fireEvent.click(getByTestId("toggleMeasurementSystemButton"));
+
+        expect(getByText("Measurement System : US")).toBeInTheDocument();
     });
 
 });
--- src/pages/RecipeSearch/RecipeSearch.test.js
+++ src/pages/RecipeSearch/RecipeSearch.test.js
@@ -1,22 +1,67 @@
 import React from "react";
 import RecipeSearch from "./RecipeSearch";
 import {HashRouter as Router} from "react-router-dom";
-import {render, fireEvent} from "@testing-library/react";
+import {render, fireEvent, wait} from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
 
 describe("<RecipeSearch/>" , () => {
     test("User is able to search for a recipe", () => {
+        const {getByTestId, getByText} = render(
+            <Router>
+                <RecipeSearch/>
+            </Router>
+        );
 
+        fireEvent.change(getByTestId("searchTextInput"), { target: {value : "Cheese"}});
+
+        fireEvent.click(getByTestId("searchButton"));
+
+        expect(getByTestId("cheeseburgerId")).toBeInTheDocument();
     });
 
     test("No results show for invalid ingredient search", () => {
-        
+        const {getByTestId, getByText} = render(
+            <Router>
+                <RecipeSearch/>
+            </Router>
+        );
+
+        fireEvent.change(getByTestId("searchTextInput"), { target: {value : "Non existent ingredient"}});
+
+        fireEvent.click(getByTestId("searchButton"));
+
+        expect(getByTestId("noResultsText")).toBeInTheDocument();
     });
 
     test("Results show for valid ingredient search", () => {
-        
+        const {getByTestId, getByText} = render(
+            <Router>
+                <RecipeSearch/>
+            </Router>
+        );
+
+        fireEvent.change(getByTestId("searchTextInput"), { target: {value : "Cheese"}});
+
+        fireEvent.click(getByTestId("searchButton"));
+
+        expect(getByTestId("cheeseburgerId")).toBeInTheDocument();
     });
 
-    test("User is taken to another page after clicking button on table row", () => {
+    test("User is taken to another page after clicking button on table row", async () => {
+        const {getByTestId, getByText} = render(
+            <Router>
+                <RecipeSearch/>
+            </Router>
+        );
 
+        fireEvent.change(getByTestId("searchTextInput"), { target: {value : "Cheese"}});
+
+        fireEvent.click(getByTestId("searchButton"));
+
+        await wait(async () => expect(getByTestId("loadingSpinner")).not.toBeInTheDocument());
+
+        fireEvent.click(getByTestId("linkButton"));
+
+        expect(getByTestId("recipeSearchHeader")).not.toBeInTheDocument();
     });
 });
