3973206ac1d1a87d7b516ccf38ca8fbd4808c4fe
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-04 22:24:22 -0400

    sketched test for recipe info page

---
+++ src/pages/RecipeInfo/RecipeInfo.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RecipeInfo from "./RecipeInfo";
+import {HashRouter as Router} from "react-router-dom";
+import {render, fireEvent} from "@testing-library/react";
+
+describe("<RecipeInfo/>" , () => {
+    test("User is able toggle between US and metric measurements", () => {
+
+    });
+
+});
